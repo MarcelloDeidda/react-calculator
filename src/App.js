86a620dd09fb398fd3b27e0f82bdf255386f3c3d
MarcelloDeidda <marcello35@hotmail.it>
2023-03-27 21:32:36 +0100

    Adding files to repository

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+const numberToWords = require("number-to-words")
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [expression, setExpression] = useState("0");
+
+  const digits = () => {
+    const buttons = []
+    for (let i = 1; i < 10; i++) {
+      buttons.push(
+        <button
+          id={numberToWords.toWords(i)}
+          key={numberToWords.toWords(i)}
+          onClick={() => updateExpression(i.toString())}>
+          {i}
+        </button>
+      );
+    }
+    return buttons;
+  }
+
+  const operators = ["/", "*", "-", "+", "."];
+
+  const updateExpression = value => {
+    const numbers = expression.split(/\+|\-|\*|\//);
+    if (operators.includes(value)) {
+      if (value === "." && numbers[numbers.length - 1].includes(".")) {
+        return;
+      } else if (operators.includes(expression.slice(-1))) {
+        setExpression(expression.slice(0, -1) + value);
+        return;
+      }
+    } else {
+      if (value === "0" && expression === "0") {
+        return;
+      } else if (expression.slice(-1) === "0" && operators.slice(0, 4).includes(expression.slice(-2, -1))) {
+        setExpression(expression.slice(0, -1) + value);
+        return;
+      } else if (expression === "0") {
+        setExpression(value);
+        return;
+      }
+    }
+    setExpression(expression + value);
+  }
+
+  const calculateResult = () => {
+    if (!operators.includes(expression.slice(-1))) {
+      setExpression(eval(expression).toString());
+    }
+  }
+
+  const clearExpression = () => {
+    setExpression("0");
+  }
+
+  const clearOne = () => {
+    if (expression.length === 1) {
+      setExpression("0");
+      return;
+    }
+    setExpression(expression.slice(0, -1));
+  }
+
+  return <div id="App">
+    <div id="calculator">
+      <div id="display">{expression}</div>
+      <div class="operators">
+        <button id="clear" onClick={clearExpression}>AC</button>
+        <button id="clearOne" onClick={clearOne}>C</button>
+        <button id="divide" onClick={() => updateExpression("/")}>/</button>
+        <button id="multiply" onClick={() => updateExpression("*")}>*</button>
+        <button id="subtract" onClick={() => updateExpression("-")}>-</button>
+        <button id="add" onClick={() => updateExpression("+")}>+</button>
+      </div>
+      <div class="digits">
+        {digits()}
+      </div>
+      <div class="last-line">
+        <button id="zero" onClick={() => updateExpression("0")}>0</button>
+        <button id="decimal" onClick={() => updateExpression(".")}>.</button>
+        <button id="equals" onClick={calculateResult}>=</button>
+      </div>
     </div>
-  );
+  </div>
 }
 
 export default App;
